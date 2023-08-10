6d8189b618732c0d767c8fc5dea8f3fa910dd683
marou <ga86paf@mytum.de>
2023-08-10 04:01:44 +0200

    .

---
+++ train.py
@@ -0,0 +1,217 @@
+import os
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import nibabel as nib
+from torch.utils.data import Dataset, DataLoader, Subset
+import torch.nn.functional as F
+from torch.cuda.amp import GradScaler, autocast
+from tqdm import tqdm
+
+from generative.inferers import LatentDiffusionInferer
+from generative.losses.adversarial_loss import PatchAdversarialLoss
+from generative.losses.perceptual import PerceptualLoss
+from generative.networks.nets import AutoencoderKL, DiffusionModelUNet, PatchDiscriminator
+from generative.networks.schedulers import DDPMScheduler
+
+class NiftiDataset(Dataset):
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+        self.nii_files = [os.path.join(root_dir, f) for f in os.listdir(root_dir) if f.endswith('.nii')]
+        self.slices = []
+
+        # Load all slices from all nii files
+        for nii_path in self.nii_files:
+            img = nib.load(nii_path)
+            image_data = img.get_fdata()
+            image_data = np.moveaxis(image_data, -1, 0)  # convert from HxWxD to DxHxW
+            image_data = image_data.astype(np.float32)  # convert data from int16 to float32 if needed
+            image_data /= 300  # normalize data
+
+            # Crop slices to 256x256
+            images = []
+            for img in image_data:
+                start_x = (img.shape[0] - 256) // 2
+                start_y = (img.shape[1] - 256) // 2
+                img_cropped = img[start_x:start_x+256, start_y:start_y+256]  # crop image
+                img_tensor = torch.from_numpy(img_cropped).unsqueeze(0)  # convert image to tensor and add channel dimension
+                images.append(img_tensor)
+
+            self.slices.extend(images)  # add these images to the list of all slices
+
+    def __len__(self):
+        return len(self.slices)  # return the total number of slices
+
+    def __getitem__(self, idx):
+        return self.slices[idx]  # return a single slice
+
+
+root_dir = './datasets/raw'
+dataset = NiftiDataset(root_dir=root_dir)
+
+validation_split = 0.2
+dataset_size = len(dataset)
+validation_size = int(validation_split * dataset_size)
+training_size = dataset_size - validation_size
+indices = torch.randperm(len(dataset))
+train_indices = indices[:training_size]
+val_indices = indices[training_size:]
+
+train_dataset = Subset(dataset, train_indices)
+validation_dataset = Subset(dataset, val_indices)
+
+train_loader = DataLoader(train_dataset, batch_size=6, shuffle=True, num_workers=4, persistent_workers=True)
+val_loader = DataLoader(validation_dataset, batch_size=6, shuffle=False, num_workers=4, persistent_workers=True)
+
+device = torch.device("cuda")
+
+autoencoderkl = AutoencoderKL(
+    spatial_dims=2,
+    in_channels=1,
+    out_channels=1,
+    num_channels=(128, 128, 256),
+    latent_channels=3,
+    num_res_blocks=2,
+    attention_levels=(False, False, False),
+    with_encoder_nonlocal_attn=False,
+    with_decoder_nonlocal_attn=False,
+)
+autoencoderkl = autoencoderkl.to(device)
+
+perceptual_loss = PerceptualLoss(spatial_dims=2, network_type="alex")
+perceptual_loss.to(device)
+perceptual_weight = 0.001
+
+discriminator = PatchDiscriminator(spatial_dims=2, num_layers_d=3, num_channels=64, in_channels=1, out_channels=1)
+discriminator = discriminator.to(device)
+
+adv_loss = PatchAdversarialLoss(criterion="least_squares")
+adv_weight = 0.01
+
+optimizer_g = torch.optim.Adam(autoencoderkl.parameters(), lr=1e-4)
+optimizer_d = torch.optim.Adam(discriminator.parameters(), lr=5e-4)
+
+# For mixed precision training
+scaler_g = torch.cuda.amp.GradScaler()
+scaler_d = torch.cuda.amp.GradScaler()
+
+kl_weight = 1e-6
+n_epochs = 100
+val_interval = 10
+autoencoder_warm_up_n_epochs = 10
+
+epoch_recon_losses = []
+epoch_gen_losses = []
+epoch_disc_losses = []
+val_recon_losses = []
+intermediary_images = []
+num_example_images = 4
+
+for epoch in range(n_epochs):
+    autoencoderkl.train()
+    discriminator.train()
+    epoch_loss = 0
+    gen_epoch_loss = 0
+    disc_epoch_loss = 0
+    progress_bar = tqdm(enumerate(train_loader), total=len(train_loader), ncols=110)
+    progress_bar.set_description(f"Epoch {epoch}")
+    for step, batch in progress_bar:
+        images = batch.to(device)
+        optimizer_g.zero_grad(set_to_none=True)
+
+        with autocast(enabled=True):
+            reconstruction, z_mu, z_sigma = autoencoderkl(images)
+
+            recons_loss = F.l1_loss(reconstruction.float(), images.float())
+            p_loss = perceptual_loss(reconstruction.float(), images.float())
+            kl_loss = 0.5 * torch.sum(z_mu.pow(2) + z_sigma.pow(2) - torch.log(z_sigma.pow(2)) - 1, dim=[1, 2, 3])
+            kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
+            loss_g = recons_loss + (kl_weight * kl_loss) + (perceptual_weight * p_loss)
+
+            if epoch > autoencoder_warm_up_n_epochs:
+                logits_fake = discriminator(reconstruction.contiguous().float())[-1]
+                generator_loss = adv_loss(logits_fake, target_is_real=True, for_discriminator=False)
+                loss_g += adv_weight * generator_loss
+
+        scaler_g.scale(loss_g).backward()
+        scaler_g.step(optimizer_g)
+        scaler_g.update()
+
+        if epoch > autoencoder_warm_up_n_epochs:
+            with autocast(enabled=True):
+                optimizer_d.zero_grad(set_to_none=True)
+
+                logits_fake = discriminator(reconstruction.contiguous().detach())[-1]
+                loss_d_fake = adv_loss(logits_fake, target_is_real=False, for_discriminator=True)
+                logits_real = discriminator(images.contiguous().detach())[-1]
+                loss_d_real = adv_loss(logits_real, target_is_real=True, for_discriminator=True)
+                discriminator_loss = (loss_d_fake + loss_d_real) * 0.5
+
+                loss_d = adv_weight * discriminator_loss
+
+            scaler_d.scale(loss_d).backward()
+            scaler_d.step(optimizer_d)
+            scaler_d.update()
+
+        epoch_loss += recons_loss.item()
+        if epoch > autoencoder_warm_up_n_epochs:
+            gen_epoch_loss += generator_loss.item()
+            disc_epoch_loss += discriminator_loss.item()
+
+        progress_bar.set_postfix(
+            {
+                "recons_loss": epoch_loss / (step + 1),
+                "gen_loss": gen_epoch_loss / (step + 1),
+                "disc_loss": disc_epoch_loss / (step + 1),
+            }
+        )
+    epoch_recon_losses.append(epoch_loss / (step + 1))
+    epoch_gen_losses.append(gen_epoch_loss / (step + 1))
+    epoch_disc_losses.append(disc_epoch_loss / (step + 1))
+
+    if (epoch + 1) % val_interval == 0:
+        autoencoderkl.eval()
+        val_loss = 0
+        with torch.no_grad():
+            for val_step, batch in enumerate(val_loader, start=1):
+                images = batch.to(device)
+
+                with autocast(enabled=True):
+                    reconstruction, z_mu, z_sigma = autoencoderkl(images)
+                    # Get the first reconstruction from the first validation batch for visualisation purposes
+                    if val_step == 1:
+                        intermediary_images.append(reconstruction[:num_example_images, 0])
+
+                    recons_loss = F.l1_loss(images.float(), reconstruction.float())
+
+                val_loss += recons_loss.item()
+
+        val_loss /= val_step
+        val_recon_losses.append(val_loss)
+        print(f"epoch {epoch + 1} val loss: {val_loss:.4f}")
+progress_bar.close()
+
+# Get current date and time
+now = datetime.now()
+
+# Format date and time
+date_time = now.strftime("%Y%m%d_%H%M") # I replaced ":" with "M" to avoid file naming issues
+
+# Use date_time string in file name
+torch.save(autoencoderkl.state_dict(), f'autoencoderkl_weights_{date_time}.pth')
+torch.save(discriminator.state_dict(), f'discriminator_weights_{date_time}.pth')
+
+del discriminator
+del perceptual_loss
+torch.cuda.empty_cache()
+
+# Plot last 5 evaluations
+val_samples = np.linspace(n_epochs, val_interval, int(n_epochs / val_interval))
+fig, ax = plt.subplots(nrows=5, ncols=1, sharey=True, figsize=(10, 20))
+for image_n in range(5):
+    reconstructions = torch.reshape(intermediary_images[image_n]*300, (256 * num_example_images, 256)).T
+    ax[image_n].imshow(reconstructions.cpu(), cmap="jet")
+    ax[image_n].set_xticks([])
+    ax[image_n].set_yticks([])
+    ax[image_n].set_ylabel(f"Epoch {val_samples[image_n]:.0f}")
