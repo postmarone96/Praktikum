6badc6905f084df421e247afbad1389708ad4218
Marouane Hajri <73078594+postmarone96@users.noreply.github.com>
2023-08-27 18:36:54 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -16,23 +16,24 @@ class NiftiPreprocessor:
         self.output_file = output_file
 
     def process_and_save(self):
-        all_slices = []
-
-        for nii_path in self.nii_files:
-            img = nib.load(nii_path)
-            image_data = img.get_fdata()
-            image_data = np.moveaxis(image_data, -1, 0)
-            image_data = image_data.astype(np.float32)
-
-            for img in image_data:
-                max_value = np.max(img)
-                img /= max_value
-                img_cropped = img[0:256, 0:256]
-                all_slices.append(img_cropped)
-
-        # Save all slices to HDF5
         with h5py.File(self.output_file, 'w') as f:
-            f.create_dataset('all_slices', data=np.array(all_slices))
+            # Create resizable dataset
+            dset = f.create_dataset('all_slices', (0, 256, 256), maxshape=(None, 256, 256), dtype=np.float32)
+
+            for nii_path in self.nii_files:
+                img = nib.load(nii_path)
+                image_data = img.get_fdata()
+                image_data = np.moveaxis(image_data, -1, 0)
+                image_data = image_data.astype(np.float32)
+
+                for img in image_data:
+                    max_value = np.max(img)
+                    img /= max_value
+                    img_cropped = img[0:256, 0:256]
+
+                    # Append to the HDF5 dataset
+                    dset.resize((dset.shape[0] + 1, 256, 256))
+                    dset[-1] = img_cropped
 
 preprocessor = NiftiPreprocessor(root_dir=args.data_path, output_file=args.output_file)
 preprocessor.process_and_save()
