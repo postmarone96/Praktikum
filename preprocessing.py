11ea865603aefd6a386d7d3d8c495128dfc3d859
Marouane Hajri <73078594+postmarone96@users.noreply.github.com>
2023-08-27 18:41:10 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,7 @@ import numpy as np
 import nibabel as nib
 import h5py
 import argparse
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_path", type=str, required=True)
@@ -16,24 +17,34 @@ class NiftiPreprocessor:
         self.output_file = output_file
 
     def process_and_save(self):
-        with h5py.File(self.output_file, 'w') as f:
-            # Create resizable dataset
-            dset = f.create_dataset('all_slices', (0, 256, 256), maxshape=(None, 256, 256), dtype=np.float32)
+        # Compute total slices for the progress bar
+        total_slices = 0
+        for nii_path in self.nii_files:
+            img = nib.load(nii_path)
+            total_slices += img.shape[2]  # Assuming images are HxWxD
 
-            for nii_path in self.nii_files:
-                img = nib.load(nii_path)
-                image_data = img.get_fdata()
-                image_data = np.moveaxis(image_data, -1, 0)
-                image_data = image_data.astype(np.float32)
+        with tqdm(total=total_slices, desc="Processing", unit="slice") as pbar:
+            with h5py.File(self.output_file, 'w') as f:
+                # Create initial empty dataset
+                dset = f.create_dataset('all_slices', (0, 256, 256), maxshape=(None, 256, 256), chunks=True, compression="gzip", compression_opts=9)
 
-                for img in image_data:
-                    max_value = np.max(img)
-                    img /= max_value
-                    img_cropped = img[0:256, 0:256]
+                for nii_path in self.nii_files:
+                    img = nib.load(nii_path)
+                    image_data = img.get_fdata()
+                    image_data = np.moveaxis(image_data, -1, 0)
+                    image_data = image_data.astype(np.float32)
 
-                    # Append to the HDF5 dataset
-                    dset.resize((dset.shape[0] + 1, 256, 256))
-                    dset[-1] = img_cropped
+                    for img in image_data:
+                        max_value = np.max(img)
+                        img /= max_value
+                        img_cropped = img[0:256, 0:256]
+                        
+                        # Dynamically resizing the dataset for every slice
+                        dset.resize((dset.shape[0] + 1, 256, 256))
+                        dset[-1] = img_cropped
+
+                        # Update progress bar
+                        pbar.update(1)
 
 preprocessor = NiftiPreprocessor(root_dir=args.data_path, output_file=args.output_file)
 preprocessor.process_and_save()
