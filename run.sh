2117b2b133a92749f785eb045fec140329067aa5
Marouane Hajri <73078594+postmarone96@users.noreply.github.com>
2023-08-30 00:16:11 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Collect arguments or set defaults
+ARG1=$1
+
+# Export them as environment variables so they can be accessed within the SLURM script
+export ARG1
+export ARG2
+
+# Submit the SLURM job
+sbatch job.slurm
