ce1799c4888f0763ce5647554461d9450092f3a7
Marouane Hajri <73078594+postmarone96@users.noreply.github.com>
2023-08-30 00:48:28 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 # Collect arguments or set defaults
 ARG1=${1:-""}
-
+ARG2=${2:-""}
 # Export them as environment variables so they can be accessed within the SLURM script
 export ARG1
-
+export ARG2
 # Submit the SLURM job
-sbatch job.slurm
+sbatch train.slurm
