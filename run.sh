9b7f6d12aa100acad676ecc1e45311d3317c65f6
Marouane Hajri <73078594+postmarone96@users.noreply.github.com>
2023-08-31 03:15:34 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,18 @@
 # Collect arguments or set defaults
 ARG1=${1:-""}
 ARG2=${2:-""}
+
 # Export them as environment variables so they can be accessed within the SLURM script
 export ARG1
 export ARG2
+
+working_dir = $HOME/Praktikum/train_${ARG2}
+
+mkdir -p $WORKING_DIR
+
+cp -n $HOME/Praktikum/train.slurm $WORKING_DIR/
+
+cd $WORKING_DIR
+
 # Submit the SLURM job
-sbatch run_train_${ARG2}.slurm
+sbatch train.slurm
