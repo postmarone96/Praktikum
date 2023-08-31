d4f516c673a966ddc8cf3d9bd7bb01a8c2ea5db4
Marouane Hajri <73078594+postmarone96@users.noreply.github.com>
2023-08-31 12:45:31 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,12 @@
 # Collect arguments or set defaults
 ARG1=${1:-""}
 ARG2=${2:-""}
+ARG3=${3:-20}
 
 # Export them as environment variables so they can be accessed within the SLURM script
 export ARG1
 export ARG2
+export ARG3
  
 test -d train_${ARG2} || mkdir train_${ARG2}
 
