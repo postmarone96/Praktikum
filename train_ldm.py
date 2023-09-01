d1ab9fd4b3b23f430d6f68e0c715c5cf967c2d6b
Marouane Hajri <73078594+postmarone96@users.noreply.github.com>
2023-09-01 18:42:04 +0200

    Update train_ldm.py

--- train_ldm.py
+++ train_ldm.py
@@ -97,7 +97,7 @@ optimizer = torch.optim.Adam(unet.parameters(), lr=1e-4)
 scaler = GradScaler()
 autoencoderkl = AutoencoderKL(spatial_dims=2, in_channels=1, out_channels=1, num_channels=(128, 128, 256), latent_channels=3, num_res_blocks=2, attention_levels=(False, False, False), with_encoder_nonlocal_attn=False, with_decoder_nonlocal_attn=False)
 vae_path = glob.glob('autoencoderkl_weights_*.pth')
-vae_model = torch.load(vae_path)
+vae_model = torch.load(vae_path[0])
 autoencoderkl.load_state_dict(vae_model)
 scheduler = DDPMScheduler(num_train_timesteps=1000, schedule="linear_beta", beta_start=0.0015, beta_end=0.0195)
 
