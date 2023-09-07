22178d3f610aa25bad6b32ec68fe5ee9a0c7ad96
Marouane Hajri <73078594+postmarone96@users.noreply.github.com>
2023-09-07 02:48:33 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,12 +6,16 @@ ARG2=${2:-""}
 ARG3=${3:-20}
 ARG4=${4:-0}
 ARG5=${5:-0}
+ARG6=${6:-0}
+ARG7=${7:-0}
 # Export them as environment variables so they can be accessed within the SLURM script
 export ARG1
 export ARG2
 export ARG3
 export ARG4
 export ARG5
+export ARG6
+export ARG7
 
 test -d train_${ARG2} || mkdir train_${ARG2}
 
