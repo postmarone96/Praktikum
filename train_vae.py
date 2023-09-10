0436ccca66ef9983886ab8a0fc50a99a7fe91146
Marouane Hajri <73078594+postmarone96@users.noreply.github.com>
2023-09-10 17:46:29 +0200

    Update train_vae.py

--- train_vae.py
+++ train_vae.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import monai
+import zarr
 import nibabel as nib
 from torch.utils.data import Dataset, DataLoader, Subset
 import torch.nn.functional as F
@@ -53,25 +54,24 @@ def save_checkpoint_vae(epoch, autoencoder_model, discriminator_model, optimizer
     torch.save(checkpoint, filename)
 
 print_with_timestamp("Defining NiftiDataset class")
-class NiftiHDF5Dataset(Dataset):
-    def __init__(self, hdf5_file):
-        self.hdf5_file = hdf5_file
+class NiftiZarrDataset(Dataset):
+    def __init__(self, zarr_file):
+        self.zarr_file = zarr_file
+        self.zarr_dataset = zarr.open(self.zarr_file, mode='r')['all_slices']
 
     def __len__(self):
-        with h5py.File(self.hdf5_file, 'r') as f:
-            return len(f['all_slices'])
+        return self.zarr_dataset.shape[0]
 
     def __getitem__(self, idx):
-        with h5py.File(self.hdf5_file, 'r') as f:
-            image_data = f['all_slices'][idx]
-            image_data = torch.tensor(image_data).unsqueeze(0)  # Adding a channel dimension
+        image_data = self.zarr_dataset[idx]
+        image_data = torch.tensor(image_data).unsqueeze(0)  # Adding a channel dimension
         return image_data
 
 vae_best_val_loss = float('inf')
 ldm_best_val_loss = float('inf')
 
 print_with_timestamp("Loading data")
-dataset = NiftiHDF5Dataset(hdf5_file=args.output_file)
+dataset = NiftiZarrDataset(args.output_file)
 
 validation_split = 0.2
 dataset_size = len(dataset)
