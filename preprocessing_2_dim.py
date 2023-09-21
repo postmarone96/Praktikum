c1ba3cf5148ba33ed3dfc8204e84d4a3d5ff5ef7
Marouane Hajri <73078594+postmarone96@users.noreply.github.com>
2023-09-21 08:02:08 +0200

    .

---
+++ preprocessing_2_dim.py
@@ -0,0 +1,75 @@
+import os
+import numpy as np
+import nibabel as nib
+import h5py
+import argparse
+import psutil
+
+# parser
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_path", type=str, required=True)
+parser.add_argument("--output_file", type=str, required=True)
+args = parser.parse_args()
+
+class NiftiPreprocessor:
+    def __init__(self, image_dir, annotation_dir, output_file):
+        self.image_dir = image_dir
+        self.annotation_dir = annotation_dir
+        self.image_files = sorted([os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.endswith('.nii.gz')])
+        self.annotation_files = sorted([os.path.join(annotation_dir, f) for f in os.listdir(annotation_dir) if f.endswith('.nii.gz')])
+        self.output_file = output_file
+
+    def process_and_save(self):
+        # Assuming equal number of image and annotation files
+        assert len(self.image_files) == len(self.annotation_files), "Mismatch in number of image and annotation files"
+
+        buffer_image = []
+        buffer_annotation = []
+
+        with h5py.File(self.output_file, 'w') as f:
+            dset_image = f.create_dataset('image_slices', (0, 256, 256), maxshape=(None, 256, 256), chunks=True, compression="gzip", compression_opts=9)
+            dset_annotation = f.create_dataset('annotation_slices', (0, 256, 256), maxshape=(None, 256, 256), chunks=True, compression="gzip", compression_opts=9)
+
+            for image_path, annotation_path in zip(self.image_files, self.annotation_files):
+                # Handle image
+                buffer_image.extend(self.process_single_nifti(image_path))
+                # Handle annotation
+                buffer_annotation.extend(self.process_single_nifti(annotation_path))
+
+                # Save buffer if it's big enough
+                if len(buffer_image) >= 30000:
+                    self.save_buffer_to_dataset(dset_image, buffer_image)
+                    self.save_buffer_to_dataset(dset_annotation, buffer_annotation)
+                    buffer_image.clear()
+                    buffer_annotation.clear()
+
+            # If there's any remaining data in the buffers, save them
+            if buffer_image:
+                self.save_buffer_to_dataset(dset_image, buffer_image)
+            if buffer_annotation:
+                self.save_buffer_to_dataset(dset_annotation, buffer_annotation)
+
+    def process_single_nifti(self, nii_path):
+        img = nib.load(nii_path)
+        image_data = img.get_fdata()
+        image_data = np.moveaxis(image_data, -1, 0)
+        image_data = image_data.astype(np.float32)
+        buffer = []
+        for img in image_data:
+            max_value = np.max(img)
+            img /= max_value
+            img_cropped = img[0:256, 0:256]
+            buffer.append(img_cropped)
+        return buffer
+
+    def save_buffer_to_dataset(self, dataset, buffer):
+        current_length = dataset.shape[0]
+        dataset.resize((current_length + len(buffer), 256, 256))
+        dataset[current_length:current_length + len(buffer)] = np.array(buffer)
+
+# Assuming data_path points to a parent directory that contains 'C00' and 'C02' subdirectories
+image_dir = os.path.join(args.data_path, 'C00')
+annotation_dir = os.path.join(args.data_path, 'C02')
+
+preprocessor = NiftiPreprocessor(image_dir=image_dir, annotation_dir=annotation_dir, output_file=args.output_file)
+preprocessor.process_and_save()
---
+++ train_ldm_2_dim.py
@@ -0,0 +1,251 @@
+import os
+import argparse
+import h5py
+import glob
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import monai
+from monai.utils import first
+import nibabel as nib
+from torch.utils.data import Dataset, DataLoader, Subset
+import torch.nn.functional as F
+from torch.cuda.amp import GradScaler, autocast
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from tqdm import tqdm
+from generative.inferers import LatentDiffusionInferer
+from generative.losses.adversarial_loss import PatchAdversarialLoss
+from generative.losses.perceptual import PerceptualLoss
+from generative.networks.nets import AutoencoderKL, DiffusionModelUNet, PatchDiscriminator
+from generative.networks.schedulers import DDPMScheduler
+
+# clear CUDA
+torch.cuda.empty_cache()
+
+# parser
+parser = argparse.ArgumentParser()
+parser.add_argument("--output_file", type=str, required=True)
+parser.add_argument("--lr", type=str, default=1e-4)
+args = parser.parse_args()
+
+def print_with_timestamp(message):
+    current_time = datetime.now()
+    print(f"{current_time} - {message}")
+print_with_timestamp("Starting the script")
+
+def save_checkpoint_ldm(epoch, unet, optimizer, scaler, scheduler, scheduler_lr, scale_factor, epoch_losses, val_losses, val_epochs, lr_rates, filename):
+    checkpoint = {
+        'epoch': epoch,
+        'unet_state_dict': unet.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'scaler_state_dict':scaler.state_dict(),
+        'scheduler_state_dict': scheduler.state_dict(),
+        'scheduler_lr_state_dict': scheduler_lr.state_dict(),
+        'epoch_losses': epoch_losses,
+        'val_losses': val_losses,
+        'scale_factor': scale_factor,
+        'val_epochs': val_epochs,
+        'lr_rates': lr_rates,
+    }
+    torch.save(checkpoint, filename)
+
+print_with_timestamp("Defining NiftiDataset class")
+class NiftiHDF5Dataset(Dataset):
+    def __init__(self, hdf5_file):
+        self.hdf5_file = hdf5_file
+
+    def __len__(self):
+        with h5py.File(self.hdf5_file, 'r') as f:
+            # Assuming image_slices and annotation_slices have the same length
+            return len(f['image_slices'])
+
+    def __getitem__(self, idx):
+        with h5py.File(self.hdf5_file, 'r') as f:
+            image_data = f['image_slices'][idx]
+            annotation_data = f['annotation_slices'][idx]
+
+        # Convert to PyTorch tensors
+        image_data = torch.tensor(image_data)
+        annotation_data = torch.tensor(annotation_data)
+
+        # Stack the image and annotation along the channel dimension
+        combined = torch.stack([image_data, annotation_data], dim=0)
+
+        return combined
+
+print_with_timestamp("Loading data")
+dataset = NiftiHDF5Dataset(args.output_file)
+
+ldm_best_val_loss = float('inf')
+
+validation_split = 0.2
+dataset_size = len(dataset)
+validation_size = int(validation_split * dataset_size)
+training_size = dataset_size - validation_size
+indices = torch.randperm(len(dataset))
+train_indices = indices[:training_size]
+val_indices = indices[training_size:]
+
+train_dataset = Subset(dataset, train_indices)
+validation_dataset = Subset(dataset, val_indices)
+
+print_with_timestamp("Splitting data for training and validation")
+train_loader = DataLoader(train_dataset, batch_size=10, shuffle=True, num_workers=16, persistent_workers=True)
+val_loader = DataLoader(validation_dataset, batch_size=10, shuffle=False, num_workers=16, persistent_workers=True)
+
+print_with_timestamp("Setting up device and models")
+device = torch.device("cuda")
+
+print_with_timestamp("Start setting")
+unet = DiffusionModelUNet(
+    spatial_dims=2,
+    in_channels=3,
+    out_channels=3,
+    num_res_blocks=2,
+    num_channels=(128, 256, 512),
+    attention_levels=(False, True, True),
+    num_head_channels=(0, 256, 512),
+)
+optimizer = torch.optim.Adam(unet.parameters(), lr=10**(-float(args.lr)))
+scheduler_lr = ReduceLROnPlateau(optimizer, 'min', factor=0.5, patience=40)
+scaler = GradScaler()
+autoencoderkl = AutoencoderKL(spatial_dims=2, in_channels=2, out_channels=2, num_channels=(128, 128, 256), latent_channels=3, num_res_blocks=2, attention_levels=(False, False, False), with_encoder_nonlocal_attn=False, with_decoder_nonlocal_attn=False)
+
+vae_path = glob.glob('vae_model_*.pth')
+vae_model = torch.load(vae_path[0])
+autoencoderkl.load_state_dict(vae_model['autoencoder_state_dict'])
+
+scheduler = DDPMScheduler(num_train_timesteps=1000, schedule="linear_beta", beta_start=0.0015, beta_end=0.0195)
+unet = unet.to(device)
+autoencoderkl = autoencoderkl.to(device).half()
+
+start_epoch = 0
+checkpoint_path = glob.glob('ldm_checkpoint_epoch_*.pth')
+if checkpoint_path:
+    checkpoint = torch.load(checkpoint_path[0])
+    start_epoch = checkpoint['epoch'] + 1
+    scaler.load_state_dict(checkpoint['scaler_state_dict'])
+    unet.load_state_dict(checkpoint['unet_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+    scheduler_lr.load_state_dict(checkpoint['scheduler_lr_state_dict'])
+    epoch_losses = checkpoint['epoch_losses']
+    val_losses = checkpoint['val_losses']
+    val_epochs = checkpoint['val_epochs']
+    lr_rates = checkpoint['lr_rates']
+    print_with_timestamp(f"Resuming from epoch {start_epoch}...")
+else:
+    epoch_losses = []
+    val_losses = []
+    val_epochs = []
+    lr_rates = []
+
+
+n_epochs = 200
+val_interval = 2
+check_data = next(iter(train_loader))
+with torch.no_grad():
+    with autocast(enabled=True):
+        z = autoencoderkl.encode_stage_2_inputs(check_data.to(device))
+print(f"Scaling factor set to {1/torch.std(z)}")
+scale_factor = 1 / torch.std(z)
+
+inferer = LatentDiffusionInferer(scheduler, scale_factor=scale_factor)
+
+for epoch in range(start_epoch, n_epochs):
+    unet.train()
+    autoencoderkl.eval()
+    epoch_loss = 0
+    progress_bar = tqdm(enumerate(train_loader), total=len(train_loader), ncols=70)
+    progress_bar.set_description(f"Epoch {epoch}")
+    for step, batch in progress_bar:
+        images = batch.to(device)
+        optimizer.zero_grad(set_to_none=True)
+        with autocast(enabled=True):
+            z_mu, z_sigma = autoencoderkl.encode(images)
+            z = autoencoderkl.sampling(z_mu, z_sigma)
+            noise = torch.randn_like(z).to(device)
+            timesteps = torch.randint(0, inferer.scheduler.num_train_timesteps, (z.shape[0],), device=z.device).long()
+            noise_pred = inferer(
+                inputs=images, diffusion_model=unet, noise=noise, timesteps=timesteps, autoencoder_model=autoencoderkl
+            )
+            loss = F.mse_loss(noise_pred.float(), noise.float())
+
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+        epoch_loss += loss.item()
+
+        progress_bar.set_postfix({"loss": epoch_loss / (step + 1)})
+    epoch_losses.append(epoch_loss / (step + 1))
+
+    if epoch % val_interval == 0 and epoch > 0:
+        val_epochs.append(epoch)
+        unet.eval()
+        val_loss = 0
+        with torch.no_grad():
+            for val_step, batch in enumerate(val_loader, start=1):
+                images = batch.to(device)
+
+                with autocast(enabled=True):
+                    z_mu, z_sigma = autoencoderkl.encode(images)
+                    z = autoencoderkl.sampling(z_mu, z_sigma)
+                    noise = torch.randn_like(z).to(device)
+                    timesteps = torch.randint(
+                        0, inferer.scheduler.num_train_timesteps, (z.shape[0],), device=z.device
+                    ).long()
+                    noise_pred = inferer(
+                        inputs=images,
+                        diffusion_model=unet,
+                        noise=noise,
+                        timesteps=timesteps,
+                        autoencoder_model=autoencoderkl,
+                    )
+                    loss = F.mse_loss(noise_pred.float(), noise.float())
+                val_loss += loss.item()
+        val_loss /= val_step
+        scheduler_lr.step(val_loss)
+        lr_unet = optimizer.param_groups[0]['lr']
+        lr_rates.append(lr_unet)
+        val_losses.append(val_loss)
+        print(f"Epoch {epoch} val loss: {val_loss:.4f}")
+    if epoch % 5 == 0 and epoch > 0:
+        save_checkpoint_ldm(epoch, unet, optimizer, scaler, scheduler, scheduler_lr, scale_factor, epoch_losses, val_losses, val_epochs, lr_rates, f'ldm_checkpoint_epoch_{epoch}.pth')
+        if val_loss < ldm_best_val_loss:
+            ldm_best_val_loss = val_loss
+            save_checkpoint_ldm(epoch, unet, optimizer, scaler, scheduler, scheduler_lr, scale_factor, epoch_losses, val_losses, val_epochs, lr_rates, 'ldm_best_checkpoint.pth')
+    if epoch > val_interval:
+        fig, ax1 = plt.subplots(figsize=(10, 5))
+        # Plot Losses
+        color = 'tab:blue'
+        ax1.set_title('Learning Curves and Learning Rate', fontsize=20)
+        ax1.set_xlabel('Epochs', fontsize=16)
+        ax1.set_xticks(range(0, epoch + 1, 10))
+        ax1.set_ylabel('Loss', fontsize=16, color=color)
+        ax1.plot(range(epoch + 1), epoch_losses, color=color, label="Train")
+        ax1.plot(val_epochs, val_losses, 'b--', label="Validation")
+        ax1.tick_params(axis='y', labelcolor=color)
+
+        # Twin axis for learning rates
+        ax2 = ax1.twinx()
+        color = 'tab:red'
+        ax2.set_ylabel('Learning Rate', fontsize=16, color=color)
+        ax2.plot(val_epochs, lr_rates, color=color, label='Learning Rate')
+        ax2.tick_params(axis='y', labelcolor=color)
+
+        fig.tight_layout()
+        fig.legend(loc="upper right", bbox_to_anchor=(0.8,0.9))
+
+        plt.savefig('LDM_learning_curves.png')
+        plt.close()
+progress_bar.close()
+
+# Get current date and time
+now = datetime.now()
+# Format date and time
+date_time = now.strftime("%Y%m%d_%H%M")
+# Use date_time string in file name
+save_checkpoint_ldm(epoch, unet, optimizer, scaler, scheduler, scheduler_lr, scale_factor, epoch_losses, val_losses, val_epochs, lr_rates, f'ldm_model_{date_time}.pth')
+
+torch.cuda.empty_cache()
