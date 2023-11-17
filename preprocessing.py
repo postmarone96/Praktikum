1e190e26ead20963f7449e27c11da5198210d43e
Marouane Hajri <73078594+postmarone96@users.noreply.github.com>
2023-11-17 03:13:18 +0100

    .

--- preprocessing.py
+++ preprocessing.py
@@ -16,25 +16,25 @@ class NiftiPreprocessor:
     def __init__(self, raw_dir, bg_dir, gt_dir, output_file, data_size):
         self.raw = sorted([os.path.join(raw_dir, f) for f in os.listdir(raw_dir) if f.endswith('.nii.gz')])
         self.bg = sorted([os.path.join(bg_dir, f) for f in os.listdir(bg_dir) if f.endswith('.nii.gz')])
-        if data_size == 'xs':
+        if self.data_size == 'xs':
             self.gt = sorted([os.path.join(gt_dir, f) for f in os.listdir(gt_dir) if f.endswith('.nii.gz')])
         self.output_file = output_file
 
     def process_and_save(self):
         # Assuming equal number of image and annotation files
         assert len(self.raw) == len(self.bg), "Mismatch in number files"
-        if data_size == 'xs':
+        if self.data_size == 'xs':
             assert len(self.raw) == len(self.gt), "Mismatch in number gt files"
 
         buffer_raw = []
         buffer_bg = []
-        if data_size == 'xs':
+        if self.data_size == 'xs':
             buffer_gt = []
 
         with h5py.File(self.output_file, 'w') as f:
             dset_raw = f.create_dataset('raw', (0, 256, 256), maxshape=(None, 256, 256), chunks=True, compression="gzip", compression_opts=9)
             dset_bg = f.create_dataset('bg', (0, 256, 256), maxshape=(None, 256, 256), chunks=True, compression="gzip", compression_opts=9)
-            if data_size == 'xs':
+            if self.data_size == 'xs':
                 dset_gt = f.create_dataset('gt', (0, 256, 256), maxshape=(None, 256, 256), chunks=True, compression="gzip", compression_opts=9)
 
             for raw_path, bg_path, gt_path in zip(self.raw, self.bg, self.gt):
@@ -43,18 +43,18 @@ class NiftiPreprocessor:
                 # Handle bg
                 buffer_bg.extend(self.process_single_nifti(bg_path))
                 # Handle gt
-                if data_size == 'xs':
+                if self.data_size == 'xs':
                     buffer_gt.extend(self.process_single_nifti_for_masks(gt_path))
 
                 # Save buffer if it's big enough
                 if len(buffer_raw) >= 30000:
                     self.save_buffer_to_dataset(dset_raw, buffer_raw)
                     self.save_buffer_to_dataset(dset_bg, buffer_bg)
-                    if data_size == 'xs':
+                    if self.data_size == 'xs':
                         self.save_buffer_to_dataset(dset_gt, buffer_gt)
                     buffer_raw.clear()
                     buffer_bg.clear()
-                    if data_size == 'xs':
+                    if self.data_size == 'xs':
                         buffer_gt.clear()
 
             # If there's any remaining data in the buffers, save them
@@ -62,7 +62,7 @@ class NiftiPreprocessor:
                 self.save_buffer_to_dataset(dset_raw, buffer_raw)
             if buffer_bg:
                 self.save_buffer_to_dataset(dset_bg, buffer_bg)
-            if data_size == 'xs' and buffer_gt:
+            if self.data_size == 'xs' and buffer_gt:
                 self.save_buffer_to_dataset(dset_gt, buffer_gt)
 
     def process_single_nifti(self, nii_path):
