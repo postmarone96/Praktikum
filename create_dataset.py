2992b551f83ebe51fa449bf35d70feed1d3d6da3
Marouane Hajri <73078594+postmarone96@users.noreply.github.com>
2023-11-20 06:56:12 +0100

    .

--- create_dataset.py
+++ create_dataset.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     bg_dir = os.path.join(os.environ['HOME'], 'Praktikum/train_xl/data/bg')
     raw_dir = os.path.join(os.environ['HOME'], 'Praktikum/train_xl/data/raw')
     
-    copy_directory('/lustre/groups/iterm/Rami/HFD/HFD_neurons/HFD/HFD_210320_UCHL1_755_HFD_DORSAL_l_1x_35o_4x8_GRB12-7-12_17-00-17/C00/', bg_dir)
-    copy_directory('/lustre/groups/iterm/Rami/HFD/HFD_neurons/HFD/HFD_210320_UCHL1_755_HFD_DORSAL_l_1x_35o_4x8_GRB12-7-12_17-00-17/C02/', raw_dir)
+    copy_directory('/lustre/groups/iterm/Rami/HFD/HFD_neurons/HFD_210320_UCHL1_755_HFD_DORSAL_l_1x_35o_4x8_GRB12-7-12_17-00-17/C00/', bg_dir)
+    copy_directory('/lustre/groups/iterm/Rami/HFD/HFD_neurons/HFD_210320_UCHL1_755_HFD_DORSAL_l_1x_35o_4x8_GRB12-7-12_17-00-17/C02/', raw_dir)
